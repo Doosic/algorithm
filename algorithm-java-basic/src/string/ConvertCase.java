25f30f6af2e15b85c59a570660230e81510d7289
DoosicEE <strou73@naver.com>
2025-01-20 23:15:39 +0900

    알고리즘(문자열 찾기, 대소문자 변환)

---
+++ algorithm-java-basic/src/string/ConvertCase.java
@@ -0,0 +1,61 @@
+package string;
+
+import java.util.Scanner;
+
+public class ConvertCase {
+
+  /*
+      # 대소문자 변환
+
+      - 설명
+      대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+
+      - 입력 첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않는다.
+      문자열은 영어 알파벳으로만 구성되어 있다.
+
+      - 출력
+      첫 줄에는 대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 출력한다.
+   */
+
+    /*
+      # 풀이 방법들
+      1.solution 내에서 인자값을 한 단어씩 꺼내 대소문자를 판별(아스키코드 32). 이후 upperCase or rowerCase하여 반대로 변환한다.
+      2.Character 클래스에 isLowerCase() 라는 함수를 이용한다. 해당 문자가 소문자인지 판별하는 함수이다.
+
+      # 내가 선택한 방법
+      how: 1번 방법
+      why: 다른 방법이 생각나지 않음, 2번 방법은 추후에 알게되었다.
+
+      # 풀이 전략
+      1.함수 내 전달받은 매개변수를 toCharArray() 함수를 통해 char[] 로 변환한다.
+      2.문자들을 하나씩 꺼내, 아스키코드 값으로 소문자 범위인지, 대문자 범위인지 판별한다.
+      3.소문자라면 대문자로, 대문자라변 소문자로 변환하여 answer 값에 더해 출력
+
+      * ASCII 코드
+      a ~ z (97 ~ 122)
+      A ~ Z (65 ~ 90)
+   */
+
+  public static void main(String[] args) {
+    ConvertCase convertCase = new ConvertCase();
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+
+    System.out.println(convertCase.solution(str));
+  }
+
+  public String solution(String str){
+    String answer = "";
+
+    for (char c : str.toCharArray()){
+      int cNum = (int) c;
+      if(cNum >= 65 && cNum <= 90){
+        answer += (char) (cNum + 32);
+      }else{
+        answer += (char) (cNum - 32);
+      }
+    }
+
+    return answer;
+  }
+}
---
+++ algorithm-java-basic/src/string/DecodeBinaryString.java
@@ -0,0 +1,58 @@
+package string;
+
+import java.util.Scanner;
+
+public class DecodeBinaryString {
+
+  /*
+      # 문자 찾기
+
+      - 설명
+      한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하시오.
+      대소문자를 구분하지 않고, 문자열의 길이는 100을 넘지않는다.
+
+      - 입력
+      첫 줄에 문자열이 주어지고, 두 번째 줄에 문자가 주어진다.
+      문자열은 영어 알파벳으로만 구성되어 있다.
+
+      - 출력
+      첫 줄에 해당 문자의 개수를 출력한다.
+   */
+
+  /*
+      # 풀이 방법들
+      1.solution 함수 내에서 전달받은 매개변수들을 upperCase or rowerCase하여 비교
+      2.스케너에서 입력받을 때 모두 upperCase or rowerCase하여 비교
+
+      # 내가 선택한 방법
+      how: 1번 방법
+      why: solution 이라는 이름에 걸맞게 해당 함수에서 문제를 처리하기 위해서
+
+      - 풀이 전략
+      1.solution 함수 내 전달받은 문자열,문자 값을 대문자 또는 소문자로 변환.
+      2.toCharArray() 함수를 통해 문자열을 char[] 로 변환
+      3.forEach문을 통해 한 문자씩 비교
+   */
+
+  public static void main(String[] args) {
+    DecodeBinaryString decodeBinaryString = new DecodeBinaryString();
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    char c = sc.next().charAt(0);
+
+    System.out.println(decodeBinaryString.solution(str, c));
+  }
+
+  public int solution(String str, char t){
+    int answer = 0;
+
+    str = str.toUpperCase();
+    t = Character.toUpperCase(t);
+    for (char c : str.toCharArray()){
+      if(c == t){
+        answer ++;
+      }
+    }
+    return answer;
+  }
+}
