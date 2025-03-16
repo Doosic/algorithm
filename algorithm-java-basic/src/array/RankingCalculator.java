b625e8123643c4513b0ee41be7c51b102e5b7551
DoosicEE <strou73@naver.com>
2025-03-16 12:36:47 +0900

    뒤집은 소수, 점수계산, 등수 구하기

--- algorithm-java-basic/src/array/RankingCalculator.java
+++ algorithm-java-basic/src/array/RankingCalculator.java
@@ -1,7 +1,61 @@
 package array;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class RankingCalculator {
     /*
         # 등수 구하기
+
+        N명의 학생의 국어점수가 입력되면 각 학생의 등수를 입력된 순서대로 출력하는 프로그램을 작성하세요.
+        같은 점수가 입력될 경우 높은 등수로 동일 처리한다.
+        즉 가장 높은 점수가 92점인데 92점이 3명 존재하면 1등이 3명이고 그 다음 학생은 4등이 된다.
+
+        - 입력
+        첫 줄에 N(3<=N<=100)이 입력되고, 두 번째 줄에 국어점수를 의미하는 N개의 정수가 입력된다.
+
+        - 출력
+        입력된 순서대로 등수를 출력한다.
      */
+
+  /*
+      - 고민
+      나보다 더 큰 수들을 체크하여 내가 몇등인지 알아내는 풀이방법.
+      2중 for문을 사용하여 자신의 점수보다 높은 점수가 나올 때 count를 더해준다.
+      5명이 참가했을 경우 동일한 점수인 학생들 즉, 1등이 3명이라면 2등은 존재하지 않고, 바로 4등, 5등이 나와야한다.
+      때문에 자기 자신일때와 같은 값을 가진 값들은 count를 더하지 않는다.
+
+   */
+
+  public ArrayList<Integer> solution(int n, int[] arr) {
+    ArrayList<Integer> answer = new ArrayList<>();
+    int count = 1;
+
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        if(arr[i] < arr[j]){
+          count++;
+        }
+      }
+      answer.add(count);
+      count = 1;
+    }
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    RankingCalculator rankingCalculator = new RankingCalculator();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
+    for(int i = 0; i < n; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    for(int x : rankingCalculator.solution(n, arr)){
+      System.out.print(x + " ");
+    }
+  }
 }
--- algorithm-java-basic/src/array/ReversedPrime.java
+++ algorithm-java-basic/src/array/ReversedPrime.java
@@ -1,8 +1,83 @@
 package array;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ReversedPrime {
 
     /*
         # 뒤집은 소수
+
+        - 설명
+        N개의 자연수가 입력되면 각 자연수를 뒤집은 후 그 뒤집은 수가 소수이면 그 소수를 출력하는 프로그램을 작성하세요.
+        예를 들어 32를 뒤집으면 23이고, 23은 소수이다. 그러면 23을 출력한다. 단 910를 뒤집으면 19로 숫자화 해야 한다.
+        첫 자리부터의 연속된 0은 무시한다.
+
+        - 입력
+        첫 줄에 자연수의 개수 N(3<=N<=100)이 주어지고, 그 다음 줄에 N개의 자연수가 주어진다.
+        각 자연수의 크기는 100,000를 넘지 않는다.
+
+        - 출력
+        첫 줄에 뒤집은 소수를 출력합니다. 출력순서는 입력된 순서대로 출력합니다.
      */
+
+
+  public boolean isPrime(int num){
+    // 1은 제외
+    if(num == 1){
+      return false;
+    }
+    // 2부터 자기 자신 전까지 약수가 존재하는지 검사
+    for(int i = 2; i < num; i++){
+      if(num % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+  /*
+      * 숫자 뒤집기 공식 ex) 1230
+      - res = res * 10 + t
+        0 = 0 * 10 + 0
+        3 = 0 * 10 + 3
+        32 = 3 * 10 + 2
+        321 = 32 * 10 + 1
+   */
+  public ArrayList<Integer> solution(int n, int[] arr){
+    ArrayList<Integer> answer = new ArrayList<>();
+    for(int i = 0; i < n; i++){
+      int tmp = arr[i];
+      int res = 0;
+      // 숫자를 뒤집어준다.
+      while (tmp > 0){
+        int t = tmp % 10; // 1230 % 10 = 0
+        res = res * 10 + t; // 0 * 0 + 0 = 0
+        tmp = tmp/10; // 1230 / 10 = 123(다음 숫자)
+      }
+      // 소수인지 검사한 후 소수라면 담아준다.
+      if(isPrime(res)){
+        answer.add(res);
+      }
+
+    }
+
+    return answer;
+  }
+
+  public static void main(String[] args) {
+    ReversedPrime reversedPrime = new ReversedPrime();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
+    for(int i = 0; i < n; i++){
+      arr[i] = sc.nextInt();
+    }
+
+    for(int x : reversedPrime.solution(n, arr)){
+      System.out.print(x + " ");
+    }
+
+  }
 }
