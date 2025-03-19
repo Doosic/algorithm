7c83f9528e93a18cc7155cd1c2d5fffdb3090e8f
doosic <strou73@naver.com>
2025-03-19 17:08:21 +0900

    봉우리, 격자판 최대합

--- algorithm-java-basic/src/array/GridMaxSum.java
+++ algorithm-java-basic/src/array/GridMaxSum.java
@@ -1,7 +1,73 @@
 package array;
 
+import java.util.Scanner;
+
 public class GridMaxSum {
     /*
         # 격자판 최대합
+
+        - 설명
+        5*5 격자판에 숫자가 적혀있다.
+        N*N 격자판이 주어지면 각 행의 합, 각열의 합, 두 대각선의 합 중 가장 큰 합을 출력한다.
+
+        - 입력
+        첫 줄에 자연수 N이 주어진다.(2<=N<=50)
+        두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는다.
+
+        - 출력
+        최대합을 출력한다.
+
+
+        # 풀이방법
+        1.각 열,행,대각선의 max 값을 저장하는 변수를 만든다.
+        2.반복문을 각각의 max 값을 저장
+        3.열, 행, 대각선의 max 값들 중 가장 큰 값을 리턴
      */
+
+    public int solution(int n, int[][] arr){
+        int answer = 0;
+
+        int column = 0;
+        int row = 0;
+        int cross = 0;
+        int reverseCross = 0;
+        for(int i =  0; i < n; i++){
+            // 임시저장 변수는 반복문이 반복될 때 마다 초기화
+            int tmpColumn = 0;
+            int tmpRow = 0;
+            for(int j = 0; j < n; j++){
+                // 열의 합
+                tmpColumn += arr[i][j];
+                // 행의 합
+                tmpRow += arr[j][i];
+            }
+
+            // 열과 행에 가장 큰 값을 저장
+            column = Math.max(tmpColumn, column);
+            row = Math.max(tmpRow, row);
+            // 대각선
+            cross += arr[i][i];
+            reverseCross += arr[(n-i)-1][(n-i)-1];
+        }
+
+        int columnRowLine = Math.max(column, row);
+        int crossLine = Math.max(cross, reverseCross);
+        answer = Math.max(columnRowLine, crossLine);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        GridMaxSum gridMaxSum = new GridMaxSum();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(gridMaxSum.solution(n, arr));
+    }
 }
--- algorithm-java-basic/src/array/PeakFinder.java
+++ algorithm-java-basic/src/array/PeakFinder.java
@@ -1,7 +1,81 @@
 package array;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PeakFinder {
     /*
         # 봉우리
+
+        - 설명
+        지도 정보가 N*N 격자판에 주어집니다. 각 격자에는 그 지역의 높이가 쓰여있습니다.
+        각 격자판의 숫자 중 자신의 상하좌우 숫자보다 큰 숫자는 봉우리 지역입니다. 봉우리 지역이 몇 개 있는 지 알아내는 프로그램을 작성하세요.
+        격자의 가장자리는 0으로 초기화 되었다고 가정한다.
+        만약 N=5 이고, 격자판의 숫자가 다음과 같다면 봉우리의 개수는 10개입니다.
+
+        - 입력
+        첫 줄에 자연수 N이 주어진다.(2<=N<=50)
+        두 번째 줄부터 N줄에 걸쳐 각 줄에 N개의 자연수가 주어진다. 각 자연수는 100을 넘지 않는다.
+
+        - 출력
+        봉우리의 개수를 출력하세요.
+
+
+        # 풀이방법
+        1.격자구성은 (N+2) * (N+2) 로 구성한다.
+        2.1부터 N까지 반복하며 입력값들을 받는다.
+        ex) n = 5, 입력값 5,3,7,2,3 일 경우. for(int i = 1; i <= n; i++)
+        arr[0] = 0; // 기본값 초기화
+        arr[1] = 5; // 할당
+        arr[2] = 3; // 할당
+        arr[3] = 7; // 할당
+        arr[4] = 2; // 할당
+        arr[5] = 3; // 할당
+        arr[6] = 0; // 기본값 초기화
+        (가장자리는 0으로 초기화/자바 int 기본값 0 할당되어있으므로 따로 안해줘도 된다.)
+        3.배열을 순회하며 상,하,좌,우의 값들과 비교하여 봉우리 개수를 체크한다.
      */
+
+    public int solution(int n, int[][] arr){
+        int answer = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                // 상
+                boolean up = arr[i][j] > arr[i-1][j];
+                // 하
+                boolean down = arr[i][j] > arr[i+1][j];
+                // 좌
+                boolean left = arr[i][j] > arr[i][j-1];
+                // 우
+                boolean right = arr[i][j] > arr[i][j+1];
+                if(up && down && left && right ){
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PeakFinder pf = new PeakFinder();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n+2][n+2];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+//        System.out.println(Arrays.toString(arr[0]));
+//        System.out.println(Arrays.toString(arr[1]));
+//        System.out.println(Arrays.toString(arr[2]));
+//        System.out.println(Arrays.toString(arr[3]));
+//        System.out.println(Arrays.toString(arr[4]));
+//        System.out.println(Arrays.toString(arr[5]));
+//        System.out.println(Arrays.toString(arr[6]));
+
+        System.out.println(pf.solution(n, arr));
+    }
 }
